c111db8a1ef967486288d433b94f4fa02e7d5a22
charneff <charlotte.neff@gmail.com>
2019-10-01 21:34:48 -0500

    more stuff

--- items-api/items/src/components/Items.js
+++ items-api/items/src/components/Items.js
@@ -13,9 +13,30 @@ const Items = (props) => {
         const item = props.items.find(d => d.id === parseInt(e.target.id))
         props.deleteItem(item)
     }
+    debugger;
 
-    const completedItems = props.completedItems.map((item, i) => <li key={ i }><b>{item.title} </b><input id={item.id}  type="hidden"></input><br/><button id={item.id} type="button" onClick={deleteItem}>Delete</button><br/><br/></li>)
-    const incompleteItems = props.incompleteItems.map((item, i) => <li key={ i }><b>{item.title} </b><input type="checkbox" id={item.id} onChange={complete}></input><br/><button id={item.id} type="button" onClick={deleteItem}>Delete</button><br/><br/></li>)
+    const sortedComplete = props.completedItems.sort(function(a,b) {
+        var aTitle = a.title
+        var bTitle = b.title
+        if (aTitle < bTitle) 
+            { return -1} 
+        if (aTitle > bTitle)
+            { return 1}  
+        return 0
+    })
+
+    const sortedIncomplete = props.incompleteItems.sort(function(a,b) {
+        var aTitle = a.title
+        var bTitle = b.title
+        if (aTitle < bTitle) 
+            { return -1} 
+        if (aTitle > bTitle)
+            { return 1}  
+        return 0
+    })
+    
+    const completedItems = sortedComplete.map((item, i) => <li key={ i }><b>{item.title} </b><input id={item.id}  type="hidden"></input><br/><button id={item.id} type="button" onClick={deleteItem}>Delete</button><br/><br/></li>)
+    const incompleteItems = sortedIncomplete.map((item, i) => <li key={ i }><b>{item.title} </b><input type="checkbox" id={item.id} onChange={complete}></input><br/><button id={item.id} type="button" onClick={deleteItem}>Delete</button><br/><br/></li>)
         return ( <div className="Items">
             <h1>Bucket List!</h1>
             <h2><u>To Do:</u></h2>  
@@ -38,3 +59,21 @@ const Items = (props) => {
     }
 
     export default connect(mapStateToProps, { completeItem, deleteItem, editItem })(Items)
+
+// const completeI = props.completedItems
+// const incompleteI = props.incompleteItems
+
+// function sortMe(arr) { 
+//     arr.sort(function(a,b){
+//         var aTitle = a.title
+//         var bTitle = b.title
+//         if (aTitle < bTitle) 
+//             { return -1} 
+//         if (aTitle > bTitle)
+//             { return 1}  
+//         return 0
+//     })
+// }
+
+// let sortComp = sortMe(completeI)
+// let sortInc = sortMe(incompleteI)
